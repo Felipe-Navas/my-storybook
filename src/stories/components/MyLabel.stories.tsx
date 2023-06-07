6b9bb15745f8a19bee3ca92897b57fd4541d3053
Felipe Navas <felipenavas.itec@gmail.com>
2023-06-07 18:13:38 -0300

    Adding first storie

---
+++ src/stories/components/MyLabel.stories.tsx
@@ -0,0 +1,19 @@
+import { StoryFn, Meta } from '@storybook/react'
+import { MyLabel } from '../../components/MyLabel'
+
+export default {
+  title: 'UI/MyLabel',
+  component: MyLabel,
+  tags: ['autodocs']
+} as Meta<typeof MyLabel>;
+
+const Template: StoryFn<typeof MyLabel> = (args) => <MyLabel {...args} />
+
+export const Basic = Template.bind({})
+Basic.args = {
+  size: 'normal'
+}
+
+export const AllCaps = Template.bind({})
+
+export const Secondary = Template.bind({})
