a4854f6d26ffc4ea3ffdf593a88cbea7fde41949
Felipe Navas <felipenavas.itec@gmail.com>
2023-06-22 18:11:20 -0300

    Adding docs, updating readme and deploy

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -2,6 +2,18 @@ import './myLabel.css'
 
 export interface Props {
   /**
+   * This is to capitalize all the words
+   */
+  allCaps: boolean
+  /**
+   * This is the color of the button
+   */
+  color: 'primary' | 'secondary' | 'tertiary'
+  /**
+   * This is the color of the font
+   */
+  fontColor?: string
+  /**
    * This is the message to show in the label
    */
   label: string
@@ -11,6 +23,19 @@ export interface Props {
   size: 'normal' | 'h1' | 'h2' | 'h3'
 }
 
-export const MyLabel = ({ label = 'No Label', size = 'normal' }: Props) => {
-  return <span className={`${size}`}>{label}</span>
+export const MyLabel = ({
+  allCaps = false,
+  color = 'primary',
+  fontColor = '',
+  label = 'No Label',
+  size = 'normal',
+}: Props) => {
+  return (
+    <span
+      className={`label ${size} text-${color}`}
+      style={{ color: fontColor }}
+    >
+      {allCaps ? label.toUpperCase() : label}
+    </span>
+  )
 }
--- src/stories/components/MyLabel.stories.tsx
+++ src/stories/components/MyLabel.stories.tsx
@@ -4,16 +4,42 @@ import { MyLabel } from '../../components/MyLabel'
 export default {
   title: 'UI/MyLabel',
   component: MyLabel,
-  tags: ['autodocs']
-} as Meta<typeof MyLabel>;
+  tags: ['autodocs'],
+  argTypes: {
+    size: { control: 'select', options: ['normal', 'h1', 'h2', 'h3'] },
+    color: { control: 'select', options: ['primary', 'secondary', 'tertiary'] },
+    fontColor: { control: 'color' },
+  },
+} as Meta<typeof MyLabel>
 
 const Template: StoryFn<typeof MyLabel> = (args) => <MyLabel {...args} />
 
 export const Basic = Template.bind({})
 Basic.args = {
-  size: 'normal'
+  size: 'normal',
+  allCaps: false,
 }
 
 export const AllCaps = Template.bind({})
+AllCaps.args = {
+  size: 'normal',
+  allCaps: true,
+}
 
 export const Secondary = Template.bind({})
+Secondary.args = {
+  size: 'normal',
+  color: 'secondary',
+}
+
+export const Tertiary = Template.bind({})
+Tertiary.args = {
+  size: 'normal',
+  color: 'tertiary',
+}
+
+export const CustomFontColor = Template.bind({})
+CustomFontColor.args = {
+  size: 'h1',
+  fontColor: '#5517ac',
+}
